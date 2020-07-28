3fbdea7b703cf9ce026b904509eca1ca599f1eef
Manuel Coronel <coronel.andrade.manuel@gmail.com>
2020-07-28 17:32:23 -0500

    POO

--- Using Databases with Python/ObjectClassOne.py
+++ Using Databases with Python/ObjectClassOne.py
@@ -2,6 +2,7 @@
 class PartyAnimal :
     x = 0
 
+# self == this. en java
     def party(self):
         self.x = self.x + 1
         print("So far",self.x)
@@ -10,4 +11,4 @@ an = PartyAnimal()
 
 an.party()
 an.party()
-an.party()        
+an.party()
